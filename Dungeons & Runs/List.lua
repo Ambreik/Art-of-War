42f10600721829890a01a5142fa1537b7d4001bd
Ambreik <gr3bla@gmail.com>
2025-09-26 19:30:04 +0100

    Add files via upload

---
+++ Dungeons & Runs/List.lua
@@ -0,0 +1,16 @@
+			Dungeons
+
+*The Filth Below - medium size dungeon, located in "Dendera - Dravenhold City" , underneath the city.
+*Blackrock Grotto - small size dungeon, located in "Dendera - Forest of Trevale". Is the first "Indoor" that players will face.		
+*Rugged Mine - small size dungeon, located in "Dendera - Isfield Territory".
+*Sundry Lake - small size dungeon, with complex mechanics, located in "Fanthe'kar - Anghal Desert - Black Sands".
+*Barh Khaon Cave - medium size dungeon, with complex mechanics, located in "Fanthe'kar - Golden Fields - Alddan Field".
+*Tomb of Zehir, the Sun-Eater - medium size dungeon, with complex mechanics, ocated in "Fanthe'kar - Anghal Desert".
+			Runs
+	
+* - small size run, located in "Dendera - Isle of Den". Is the first "Indoor" that players will face.
+
+
+			Instances
+			
+*
