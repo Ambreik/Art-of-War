f17f70022a798be605634f0669cf91d8e9d6d8a6
Ambreik <gr3bla@gmail.com>
2025-09-26 17:52:24 +0100

    Mystic Stones - All files concerning "Mystic Stones" mechanics

---
+++ GAME/Mystic Stones/Mystic Stones.lua
@@ -0,0 +1,10 @@
+			Mystic Stone
+			
+		Description 
+	
+	Mystic Stones are statues that represent different characters or important monsters or characteristics of a region or territories. Can be targeted
+	
+	Mechanics 
+	
+	List of Mystic Stones 
+
