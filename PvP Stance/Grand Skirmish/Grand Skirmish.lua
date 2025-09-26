2d0c452d92e1199830faefe46c94ed9117907fd6
Ambreik <gr3bla@gmail.com>
2025-09-26 19:26:04 +0100

    Add files via upload

---
+++ PvP Stance/Grand Skirmish/Grand Skirmish.lua
@@ -0,0 +1,27 @@
+			Grand Skirmish
+			
+	Grand Skirmish or Party PvP is similar with "Heroes' Ascent" but require a party of 2 or 3 people and is accessed from "Rypere Phil" located in 
+"Middlean Islands \Capital City".
+	Once a character reached 70 level, can start adventure as being part of a group , where is able to request or to be requested for a duel maximum 10 times a 
+day. Before being able to do this, first must create a party at the same NPC, where have to type down the name of his members that he want to group with. Once
+this was completed, a checking message is sended to those characters with a 30 seconds confirmation. A window pop-up in the middle of the screen that cannot be moved
+away nor closed until answer with "ACCEPT" or "DECLINE". Once party is set, leader can access second line from "Rypere Phil" where can start seeking for another party
+to duel with. When one was found, this group is asked if want to accept the skirmish by "ACCEPT" or "DECLINE" botton. Other way is to type down as the same NPC the
+name of the leader of the target party. end 
+
+	For each participation, every character gain by default 2 "Insignia" plus an extra for their performances such as 
+*Winner gain 13 "Insignia"
+*loser gain 5 "Insignia".
+
+	Mechanics are the same as "Heroes' Ascent".
+	Once duel was accepted in 5 seconds both participants will be teleported to the arena and the duel will start in 10 seconds.
+	Duration of a duel is not capped but if any of the participants cause any damage to the opponent in a minimum of 10 seconds, a worning is applied for next 5 
+seconds where after duel is over in a draw and both receive 0 "Insignia" and lose once attempt.
+	Once the duel is over, both will be teleported to the location that was before. end
+	
+	A character cannot be seen by the system if is not in an "Outdoor MAP" this mean outside of a "Dungeon/Run/Instance" or "Travelling Ship". If are on a flying 
+mount, on return they will be near to the location but on ground and apply a "Protection Arena Buff" if is around "Aggro" monsters with a duration of 10 seconds.end
+
+
+	Heroes' Ascent is part of a big PvP Ranking System, where all players with 70+ level are placed. Each player gain "Insignia" that represent their points in 
+PvP. This ranking consist in any PvP stance such as "Arena" , "Hall of Heroes" , "World Battlefield" and open world battles.end end end 
---
+++ PvP Stance/Heroes' Ascent/Heroes' Ascent.lua
@@ -0,0 +1,36 @@
+			Heroes' Ascent
+			
+	This is a daily PvP contest that is accessed from "Hiel Mall" located in "Aarong Forest/Forest Albina" and also in "Arena of Valor" in "Capital City".
+	Once a character reach level 70, can start adventure as a duelist in PvP Arena Stance, where is able to request or to be requested for a duel maximum 10 times a 
+day where the number of participations is increased every 10 levels with 2 with a cap of 25 maximum duels at level 120. For each participation, every character gain 
+by default 2 "Insignia" plus an extra for their performances such as 
+*Winner gain 8 "Insignia"
+*loser gain 3 "Insignia".
+
+		How is working 
+	Once a player decided that he want to participate, simple find "Hiel Mall" and speak with him. Choose the quest line called "I want to participate in Arena". 
+First time he will explain the rules and unlock another quest called "Daily Arena" where the character will have to choose from two sections :
+*Call for a duel, where here character have to type down the name of the opponent and wait for approval if the opponent accept it or decline if refuse. This is done
+by an automatic system that send a message to the target's screen with a message as : "[player name] challenged you to a duel" with two bottons bellow, one for 
+"ACCEPT" and one for "DECLINE". This message will dissapear after one minute. Also a times is shown under this message.
+*Take the call yourself, where this NPC will look anyone close to character level and placement and send an automatic message to more opponents. First character who
+accept it, will be the one who face the initiator in the arena.
+	Once duel was accepted in 5 seconds both participants will be teleported to the arena and the duel will start in 10 seconds.
+	
+		In the Arena
+	In the arena, all characters are able to use most of the buffs, potions, runes and PvP pets.
+	Is blocked by system all potions, food or drinks that can refill HP BAR, MANA BAR, ENERGY BAR or RAGE BAR.
+	Is forbidden use of mounts in arena.
+	Once in the arena, every character lose their buffs received from other characters outside such as "Shaman or Warlock Buffs" or other type of buffs that can help
+in PvP stance here. Once the duel os over, those buffs are applied by on the character with the remaining duration before the duel started. Also is similar to other
+buffs such "Environmental Buffs".
+	Duration of a duel is not capped but if any of the participants cause any damage to the opponent in a minimum of 10 seconds, a worning is applied for next 5 
+seconds where after duel is over in a draw and both receive 0 "Insignia" and lose once attempt.
+	Once the duel is over, both will be teleported to the location that was before. 
+	
+	A character cannot be seen by the system if is not in an "Outdoor MAP" this mean outside of a "Dungeon/Run/Instance" or "Travelling Ship". If are on a flying 
+mount, on return they will be near to the location but on ground and apply a "Protection Arena Buff" if is around "Aggro" monsters with a duration of 10 seconds.
+
+
+	Heroes' Ascent is part of a big PvP Ranking System, where all players with 70+ level are placed. Each player gain "Insignia" that represent their points in 
+PvP. This ranking consist in any PvP stance such as "Grand Skirmish" , "Hall of Heroes" , "World Battlefield" and open world battles.
