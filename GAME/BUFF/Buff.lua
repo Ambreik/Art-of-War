e60d1c8907ef5d848cae29ad138aad42620aeae0
Ambreik <gr3bla@gmail.com>
2025-09-26 17:18:30 +0100

    BUFF - All files relevant to "Buff" mechanics

---
+++ GAME/BUFF/Buff.lua
@@ -0,0 +1,138 @@
+			Buffs
+	
+	In the "World Game", buffs refer to temporary beneficial effects that players can apply to enhance stats, increase attack, grant protection, or remove status 
+effects. Buffs can vary based on how they are acquired and their specific effects. They fall into two main categories:
+
+Time-Limited Buffs: These expire after a certain time.
+Environment-Dependent Buffs: These depend on the player’s surroundings and have conditions, such as remaining in a specific area.
+
+	Buff Mechanics
+*Dismissing Buffs: Players can right-click a buff icon to remove it.
+*Stacking Rules: Most buffs do not stack with similar ones, with exceptions like Heal Over Time (HoT) spells from multiple characters or different "HoT" spells from
+the same character.
+*Effects: Buffs can improve stats, increase attack, grant protection, and counteract debuffs or environmental disadvantages.
+	
+		List of notable buffs from environment
+
+	Borregar Blessing 
+How to get : spending more than 1 hour in any "Tavern" , receive this buff from the owner as a mission once you speak with him.
+Effect : Offer 5% Element Resistance for the region where character took shelter.
+Duration : permanent until change region. If character teleport out of the region that buff was applied for, effect disappear. Also this buff disappear when logout.
+Suffer debuff : No.
+UI Icon : Yes.
+
+	Spirit of the Forester
+How to get : Kill all "Special Monsters", "Bosses" and "Special Boss" (monsters found outside on maps).
+Effect : Increse all stats with +5% in "Aarong Forest" permanent.
+Duration : permanent.
+Suffer debuff : No.
+UI Icon : Yes.
+
+	Well Rest
+How to get : Spending time in tavern, doing activities or just drinking but not being AFK inside.
+Effect : Increase exp earned by killing monsters.
+Duration : until time expire.
+Suffer debuff : No.
+UI Icon : Yes.
+
+
+		List of notable buffs from Bosses and monsters
+		
+	Beater of the Pirates
+How to get : Stand against "Captain Rock, White Beard" for at least 2 minutes in PvM stance with him.
+Effect : Give +10% damage against "Undead" and 10% damage against "Pirates".
+Duration : 7 days.
+Suffer debuff : High ranked military can attack you in big cities and if you are killed, you lose this buff and 20% experience points. There is only a 20% chance of
+	being discovered by an army officer
+UI Icon : Yes.
+
+		
+		List of buffs from NPCs : 
+		
+	Angler's Blessing
+How to get : finish the quest "A favour to ask" started from "Captainess Mary Lebrande" on "Elisabeth Mighty Ship", if the news from the leter are good. This 
+buff can be received only by "Fishermen".
+Effect : Next upgrade of your fishing rod is free of cost and is 100% successfull.
+Duration : Until is used.
+Suffer Debuff : No.
+UI Icon : Yes.
+
+	Baker's Ale 
+How to get : "Waiter Looz" found on "Elisabeth Mighty Ship" sell a beverage with the same name as the buff. 
+Effect: Temporarily boosts crafting skill by 5% for one hour with a chance of 2% per drink consumed.
+Duration : 1 hour.
+Suffer Debuff : No.
+UI Icon : Yes.
+
+	Drunken Bravery
+How to get : Owner of "Red Flue Tavern" sell a beverage drink, randomly one time per day,
+Effect : Temporary Strength Boost.
+Duration : 90 minutes.
+Suffer Debuff : Reduced accuracy.
+UI Icon : Yes.
+
+	Great hand of a Bounty
+How to get : Owner of "Red Flue Tavern" sell a beverage drink, randomly one time per day,
+Effect : +5% Attack against Semi-human .
+Duration : Duration is equal with periof of a criminal being fugitive.
+Suffer Debuff : No.
+UI Icon : Yes.
+
+	Guard’s Blind Eye
+How to get : Successuflly bribe a NPC, in certain locations.
+Effect : Guards and patrol NPCs reduce detection radius by 30%.
+Duration : 5–10 minutes (scales by NPC tier bribed).
+Suffer Debuff : No.
+UI Icon : Yes.
+Flavor : “The guard pretends not to notice your presence.”
+
+	Smuggler’s Favor
+How to get : Successuflly bribe a NPC, in certain locations.
+Effect : Temporary access to hidden outlaw shops or smuggling transports.
+Duration : Until leaving the region.
+Suffer Debuff : No.
+UI Icon : Yes.
+Flavor : “Your silver buys silence… and a little extra.”
+
+	Underworld Connections
+How to get : Successuflly bribe a NPC, in certain locations.
+Effect : +10% better prices at outlaw merchants, +5% black-market success chance.
+Duration : 30 minutes.
+Suffer Debuff : No.
+UI Icon : Yes.
+Flavor : “A nod and a wink from the shadows open hidden doors.”
+
+	Bribery Momentum
+How to get : Successuflly bribe a NPC, in certain locations.
+Effect : Each successful bribe in a city reduces the cost of the next by 5% (up to -20%).
+Duration : Until leaving the city/region.
+Suffer Debuff : No.
+UI Icon : Yes.
+Flavor : “The more you grease palms, the easier hands stay open.”
+
+		List of Buff from Food and Drinks : 
+		
+	Echo Sense
+How to get : Drinking "Whispering Ale" in "The Gloomy Lanter Tavern" in the night time.
+Effect : Allows players to hear faint whispers of secrets or hints about upcoming quests from NPCs and helps players uncover hidden lore or quest leads.
+Duration : 1 hour.
+Suffer debuff : No.
+UI Icon : Yes.
+
+	Night Sight
+How to get : Drinking "Phantom’s Whisper" in "The Gloomy Lanter Tavern" in the night time.
+Effect : see in dim or dark conditions without the need for a light source
+Duration : 1 hour.
+Suffer debuff : No.
+UI Icon : Yes.
+
+
+How to get : 
+Effect : 
+Duration : 
+Suffer debuff : 
+UI Icon : 
+
+		List of Buffs from Events : 
+		
+	[go check fishing events]
