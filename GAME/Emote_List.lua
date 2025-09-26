a103a7cd7c82fd2b3410c6e03acca58dbfa56eaa
Ambreik <gr3bla@gmail.com>
2025-09-26 18:02:16 +0100

    Emote List

---
+++ GAME/Emote_List.lua
@@ -0,0 +1,170 @@
+	EmoteCommand				Anim	Voice			No Target Text 												Targeted Text
+/absent												You look absent-minded.										You look at %s absently.
+/agree												You agree.													You agree with %s.
+/amaze												You are amazed!												You are amazed by %s!
+/angry /mad						✔️					You raise your fist in anger.								You raise your fist in anger at %s.
+/apologize /sorry				✔️		✔️			You apologize to everyone. Sorry!							You apologize to %s. Sorry!
+/applaud /bravo /applause		✔️		✔️			You applaud. Bravo!											You applaud at %s. Bravo!
+/attacktarget					✔️		✔️			You tell everyone to attack something.						You tell everyone to attack %s.
+/badfeeling /bad									You have a bad feeling about this...						You have a bad feeling about %s.
+/beg							✔️		✔️			You beg everyone around you. How pathetic.					You beg %s. How pathetic.
+/blame							✔️					You blame yourself for what happened.						You blame %s for everything.
+/blush							✔️					You blush.													You blush at %s.
+/bored									✔️			You are overcome with boredom. Oh the drudgery!				You are terribly bored with %s.
+/bounce							✔️					You bounce up and down.										You bounce up and down in front of %s.
+/bow							✔️					You bow down graciously.									You bow before %s.
+/brandish						✔️					You brandish your weapon fiercely.							You brandish your weapon fiercely at %s.️️
+/brb									✔️			You let everyone know you'll be right back.					You let %s know you'll be right back.
+/breath									✔️			You take a deep breath.										You tell %s to take a deep breath.
+/burp /belch							✔️			You let out a loud belch.									You burp rudely in %s's face.
+/bye /goodbye /farewell			✔️		✔️			You wave goodbye to everyone. Farewell!						You wave goodbye to %s. Farewell!
+/cackle							✔️		✔️			You cackle maniacally at the situation.						You cackle maniacally at %s.
+/calm												You remain calm.	
+/challenge											You put out a challenge to everyone. Bring it on!			You challenge %s to a duel.
+/charge							✔️		✔️			You start to charge.	
+/cheer 							✔️		✔️			You cheer!													You cheer at %s.
+/chug												You take a mighty quaff of your beverage.					You encourage %s to chug. 
+/clap							✔️		✔			You clap excitedly.											You clap excitedly for %s.
+/cold							✔		✔			You let everyone know that you are cold.					You let %s know that you are cold.
+/commend						✔️					You commend everyone on a job well done.					You commend %s on a job well done.
+/confused						✔️		✔			You are hopelessly confused.								You look at %s with a confused look.
+/congratulate /congrats /grats	✔️		✔️			You congratulate everyone around you.						You congratulate %s.
+/cough							✔️					You let out a hacking cough.								You cough at %s.
+/coverears						✔️					You cover your ears.										You cover %s's ears.
+/cower /fear					✔️		✔️			You cower in fear.											You cower in fear at the sight of %s.
+/crack /knuckles				✔️		✔️			You crack your knuckles.									You crack your knuckles while staring at %s.
+/cringe							✔️					You cringe in fear.											You cringe away from %s.
+/crossarms						✔️					You cross your arms.										You cross your arms at %s. Hmph!
+/cry /sob 						✔️		✔️			You cry.													You cry on %s's shoulder.
+/curious						✔️					You express your curiosity to those around you.				You are curious what %s is up to.
+/curtsey						✔️					You curtsey.												You curtsey before %s.
+/dance	1						✔️					You burst into dance.										
+/dance	2						✔️					You burst into dance.										
+/dance	3						✔️					You burst into dance.										
+/dance	4						✔️					You burst into dance.										
+/dance	5						✔️					You burst into dance.										
+/disagree						✔️		✔			You disagree.												You disagree with %s.
+/threaten /threat /wrath		✔️		✔️			You threaten everyone with the wrath of doom.				You threaten %s with the wrath of doom.
+/doubt							✔️		✔️			You doubt the situation will end in your favor.		
+/drink /shindig					✔️		✔️			You raise a drink in the air before chugging it down. Cheers!	
+/duck							✔️		✔️			You duck for cover.											You duck behind %s.
+/eat /chew /feast				✔️		✔️			You begin to eat.											
+/embarrass						✔️		✔️			You flush with embarrassment.								You are embarrassed by %s.
+/encourage						✔️		✔			You encourage everyone around you.							You encourage %s.
+/enemy							✔️		✔			You warn everyone that an enemy is near.					You warn %s that an enemy is near.
+/facepalm /palm					✔					You cover your face with your palm.	
+/faint							✔					You faint.													You faint at the sight of %s.
+/fart							✔		✔			You fart loudly. Whew...what stinks?						You brush up against %s and fart loudly.
+/fidget /impatient				✔					You fidget.	
+/flee /retreat					✔️		✔️			You yell for everyone to flee!						
+/flex /strong					✔️					You flex your muscles. Oooooh so strong!	
+/flirt							✔️		✔️			You flirt.													You flirt with %s.
+/followme						✔️		✔️			You motion for everyone to follow.							You motion for %s to follow.
+/gasp							✔️					You gasp.													You gasp at %s.
+/giggle							✔️		✔️			You giggle.													You giggle at %s.
+/gloat							✔️		✔️			You gloat over everyone's misfortune.						You gloat over %s's misfortune.
+/go								✔️		✔️			You tell everyone to go.	
+/greet /greetings				✔️		✔️			You greet everyone warmly.									You greet %s warmly.
+/groan							✔️		✔️			You begin to groan in pain.									
+/growl							✔️					You growl menacingly.										You growl menacingly at %s.
+/happy /glad 					✔️		✔️			You are filled with happiness!								You are very happy with %s!
+/headache						✔️					You are getting a headache.									You are getting a headache from %s's antics.
+/healme							✔️		✔️			You call out for healing!	
+/hello /hi						✔️		✔️			You greet everyone with a hearty hello!						You greet %s with a hearty hello!
+/helpme							✔️		✔️			You cry out for help!	
+/hiccup							✔️		✔️			You hiccup loudly.	
+/highfive						✔️					You put up your hand for a high five.						You give %s a high five!
+/hiss							✔️		✔️			You hiss at everyone around you.	
+/hungry 						✔️		✔️			You are hungry!												You are hungry. Maybe %s has some food...
+/hurry							✔️					You try to pick up the pace.								You tell %s to hurry up.
+/idea												You have an idea!	
+/impressed						✔️		✔️			You clap vigorously, clearly impressed.						You clap vigorously for %s, clearly impressed.
+/incoming 						✔️		✔️			You warn everyone of incoming enemies!						You point out %s as an incoming enemy!
+/insult							✔️		✔️			You think everyone around you is a son of a motherless ogre.You think %s is the son of a motherless ogre.
+/introduce						✔️		✔️			You introduce yourself to everyone.							You introduce yourself to %s.
+/jealous						✔️					You are jealous of everyone around you.						You are jealous of %s.
+/kidding								✔️			You were just kidding!										You let %s know that you were just kidding!
+/kiss 							✔️		✔️			You blow a kiss into the wind.								You blow a kiss to %s.
+/kneel							✔️					You kneel down.												You kneel before %s.
+/laugh /lol						✔️		✔️			You laugh.													You laugh at %s.
+/laydown 						✔️					You lie down.												You lie down before %s.
+/listen							✔️					You are listening!											You listen intently to %s.
+/look							✔️					You look around.											You look at %s.
+/love							✔️					You feel the love.											You love %s.
+/luck									✔️			You wish everyone good luck.								You wish %s the best of luck.
+/massage						✔️					You need a massage!											You massage %s's shoulders.
+/meow									✔️			You meow.													You meow at %s.
+/mercy							✔️		✔️			You plead for mercy.										You plead with %s for mercy.
+/nervous						✔️					You look around nervously.									You look at %s nervously.
+/no								✔️		✔️			You clearly state, NO.										You tell %s NO. Not going to happen.
+/nosepick 						✔️					With a finger deep in one nostril, you pass the time.		You pick your nose and show it to %s.
+/objection 						✔️		✔️			You OBJECT!													You object to %s.
+/offer							✔️		✔️			You want to make an offer.									You attempt to make %s an offer they can't refuse.
+/oom							✔️		✔️			You announce that you have low mana!	
+/oops							✔️		✔️			You made a mistake.											You made a mistake.
+/openfire						✔️		✔️			You give the order to open fire.	
+/panic							✔️		✔️			You run around in a frenzied state of panic.				You take one look at %s and panic.
+/plead							✔️					You drop to your knees and plead in desperation.			You plead with %s.
+/point							✔️					You point over yonder.										You point at %s.
+/poke							✔️					You poke your belly and giggle.								You poke %s. Hey!
+/praise 						✔️					You praise the Light.										You lavish praise upon %s.
+/pray							✔️					You pray to the Gods.										You say a prayer for %s.
+/promise								✔️			You make %s a promise.
+/proud							✔️					You are proud of yourself.									You are proud of %s.
+/punch							✔️					You punch yourself.											You punch %s's shoulder.
+/raise 							✔️					You raise your hand in the air.								You look at %s and raise your hand.
+/rasp							✔️		✔️			You make a rude gesture.									You make a rude gesture at %s.
+/read							✔️					Opens a map.	
+/ready 									✔️			You let everyone know that you are ready!					You let %s know that you are ready!
+/regret							✔️					You are filled with regret.									You think that %s will regret it.
+/revenge						✔️		✔️			You vow you will have your revenge.							You vow revenge on %s.
+/roar 							✔️		✔️			You roar with bestial vigor. So fierce!						You roar with bestial vigor at %s. So fierce!
+/rude							✔️		✔️			You make a rude gesture.									You make a rude gesture at %s.
+/ruffle							✔️					You ruffle your hair.										You ruffle %s's hair.
+/sad							✔️					You hang your head dejectedly.	
+/salute							✔️		✔️			You stand at attention and salute.							You salute %s with respect.
+/scared							✔️		✔️			You are scared!												You are scared of %s.
+/scratch 						✔️					You scratch yourself. Ah, much better!						You scratch %s. How catty!
+/search							✔️					You search for something.									You search %s for something.
+/sexy							✔️					You're too sexy for your tunic...so sexy it hurts.			You think %s is a sexy devil.
+/shiver							✔️					You shiver in your boots. Chilling!							You shiver beside %s. Chilling!
+/shoo 							✔️		✔️			You shoo the measly pests away.								You shoo %s away. Be gone pest!
+/shout 							✔️		✔️			You shout.													You shout at %s.
+/shrug							✔️					You shrug. Who knows?										You shrug at %s. Who knows?
+/shy							✔️					You smile shyly.											You smile shyly at %s.
+/signal							✔️		✔️			You give the signal.										You give %s the signal.
+/silence 						✔️		✔️			You tell everyone to be quiet. Shhh!						You tell %s to be quiet. Shhh!
+/silly							✔️		✔️			You tell a joke.											You tell %s a joke.
+/sing							✔️		✔️			You burst into song.										You serenade %s with a song.
+/sit							✔️					You sit down.	
+/slap							✔️		✔️			You slap yourself across the face. Ouch!					You slap %s across the face. Ouch!
+/sleep							✔️		✔️			You fall asleep. Zzzzzzz.	
+/smack							✔️					You smack your forehead.									You smack %s upside the head.
+/smile							✔️					You smile.													You smile at %s.
+/smirk							✔️					A sly smirk spreads across your face.						You smirk slyly at %s.
+/sneak							✔️					You try to sneak away.										You try to sneak away from %s.
+/sneeze							✔️		✔️			You sneeze. Achoo!											You sneeze on %s. Achoo!
+/sniff							✔					You sniff the air around you.								You sniff %s. (Vampire/Werewolfs ONLY !)
+/stand							✔️					You stand up.	
+/surprised						✔️					You are so surprised!										You are surprised by %s's actions.
+/surrender						✔️		✔️			You surrender to your opponents.						You surrender before %s. Such is the agony of defeat...
+/suspicious						✔️					You narrow your eyes in suspicion.							You are suspicious of %s.
+/sweat							✔️					You are sweating.											You sweat at the sight of %s.
+/talk							✔️		✔️			You talk to yourself since no one else seems interested.	You want to talk things over with %s.
+/excited						✔️		✔️			You talk excitedly with everyone.							You talk excitedly with %s.
+/tap							✔️		✔️			You tap your foot. Hurry up already!						You tap your foot as you wait for %s.
+/taunt							✔️		✔️			You taunt everyone around you. Bring it fools!				You make a taunting gesture at %s. Bring it!
+/tease							✔️		✔️			You are such a tease.										You tease %s.
+/thank /ty						✔️		✔️			You thank everyone around you.								You thank %s.
+/think							✔️					You are lost in thought.									You think about %s.
+/thirsty						✔️		✔️			You are so thirsty. Can anyone spare a drink?				You let %s know you are thirsty. Spare a drink?
+/tired							✔️		✔️			You let everyone know that you are tired.					You let %s know that you are tired.
+/train							✔️		✔️			"Choo Choo Train" animation and sound	
+/truce							✔️		✔️			You offer a truce.											You offer %s a truce.
+/victory						✔️		✔️			You bask in the glory of victory.							You bask in the glory of victory with %s.
+/wait							✔️		✔️			You ask everyone to wait.									You ask %s to wait.
+/warn							✔️		✔️			You warn everyone.											You warn %s.
+/wave							✔️					You wave.													You wave at %s.
+/welcome						✔️		✔️			You welcome everyone.										You welcome %s.
+/work							✔️					You begin to work.											You work with %s.
+/yawn							✔️		✔️			You yawn sleepily.											You yawn sleepily at %s.
