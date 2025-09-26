faa6f24045f6eb2e5716287be871c4b446c5a5f8
Ambreik <gr3bla@gmail.com>
2025-09-26 18:03:47 +0100

    Key - Mechanics

---
+++ GAME/Key.lua
@@ -0,0 +1,41 @@
+			Key
+
+	There are a few types of keys:
+
+- Key for Containers  
+  An item that can be used to unlock a specific or general type of locked container. Blacksmiths can create certain keys and use them. These keys are 
+destroyed upon use. Containers requiring keys may include chests, vaults, or other locked storage systems that hold valuable items, resources, or 
+quest-related objects.
+
+- Soulbound Keys  
+  A usually soulbound item (often a quest reward) that can be used to unlock specific doors, gates, hatches, or other barriers within the game world. 
+These keys are often tied to quests or certain areas, ensuring players cannot share or sell them once acquired.
+
+- Quest Item Keys  
+  Quest item keys are used for specific quests or tasks. An example would be the "Tower Key" required for the party to open the pillars and complete a
+floor request. This key is picked up and consumed each time a "RUN" occurs. It is not exactly a standard quest item but is the only way to open the 
+cannon, and it is consumed upon use. These keys cannot be used anywhere else in the game.
+
+---
+
+		Keybag	
+
+	The keybag is a button found in the status bar, next to the inventory button.  
+	It is an expandable bag that can store various types of keys, reducing the number of inventory slots used to store them. All "World Game" 
+keys—whether picked up, crafted, or received as rewards—are stored inside this keybag for easy access.
+
+---
+
+		Lock Picking
+
+	Lock Picking is a profession learned at level 30 by any class except for "Assassins," who already have Rank I of this skill from the start of 
+their character creation. Using this profession, players can open most locked containers or doors that would otherwise require a key. However, there 
+are a few high-level dungeon doors that cannot be opened by Lock Picking.
+
+---
+
+		Mechanics
+
+	Once used, a key is destroyed in the process. A key does not guarantee a 100% success rate in opening a locked door or chest. However, the higher 
+the player's **Lock Picking** profession, the higher the chance of success when applying the skill. This applies to most locked containers, but some 
+may still require specific keys or higher-tier skills to unlock.
