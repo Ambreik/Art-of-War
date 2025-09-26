9309e0b2144db56b66068287d1f6de0a474b8e38
Ambreik <gr3bla@gmail.com>
2025-09-26 17:51:53 +0100

    Mounts - All files concerning "Mounts" mechanics

---
+++ GAME/Mounts/Legendary Horse/Legendary Horse.lua
@@ -0,0 +1,4 @@
+			Legendary Horse
+			
+	Skills : 
+*Encouragement : is a passive skill of a "Legendary Horse" that can be learned and offer immunity against most of Hard CCs once character ride already.
