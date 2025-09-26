a9cbe332a8de2b91a6bbb92341d1c31526a3a758
Ambreik <gr3bla@gmail.com>
2025-09-26 18:21:09 +0100

    Add files via upload

---
+++ Mounts/Horse/Horse System.lua
@@ -0,0 +1,43 @@
+There are 4 types of horses.
+a. Pony
+b. Armed Horse
+c. Assault Horse
+d. Legendary Horse
+
+Pony its a basic horse. You gain this mount , gift when you bring the skills.
+Whit this basic horse you are able to travel but not to use attack from it.
+Bonus : 20 ACC
+
+Armed Horse its an evolution of Pony.
+To obtain this one need to go to Horse Keeper and to claim for your first mission for horse.
+
+Mission : 
+Horse Keeper requires you to kill 100 "x" mobs in 10 minutes.If you kill all mobs in ten minutes, need to go back to the Horse Keeper and to confirm achievement. Next you need to give him the 20kk gold and the basic seal to create the new improved one and to give you the new Armed Horse.
+If you have not managed the mission , you need to wait 30 minutes before perform again the quest.
+
+Need lv. 90 minimum for use this mount.
+Bonus : 50 ACC
+
+Assault Horse its an evolution of Armed Horse.
+To obtain this horse you need to go again to the Horse Keeper and claim for the second mission for horse.
+
+Mission : 
+Horse Keeper request you to kill 500 "x" mobs in 20 minutes.If you kill all mobs in 20 minutes , need to come back to the Horse Keeper and confirm achievement.
+Next need to give him the item that you received from the quest and 50kk gold and the seal of Armed Horse to improve your old seal with new one.
+If you have not managed the mission , you need to wait 1hour before perform again the quest.
+
+Need lv. 140 minimum for this horse.
+Now you are able to perform skills from your horse. But to do that need to read some books first.
+Bonus : 100 ACC
+
+Legendary Horse its special horse. When gain lv 170 go to Horse Keeper and claim for the third mission for the horse.But first you need to complete all biologist quest and your grade need to be maximum.
+
+Mission : 
+Horse Keeper need all bosses badges. This mission not have a time request but you need to give him 5x different badges.
+*like biologist quest need to collect all badges (soul items) and don't have a rate for drop.
+When you have 5x different badges go back to the Horse Keeper and confirm achievement , then the Horse Keeper require for 100kk , Pergament of Exorcism and Crystal of Fire.
+
+Bosses : all from lv. 140+
+
+Now you have the best horse from the game. You are able to perform skills from this horse like as Assault Horse.
+Bonus : 500 ACC , +100 value attack , 10% mob resistance , +10% cool down reduction , immune to stun , poison , slow , silence , blind.
---
+++ Mounts/Mount/Mount System.lua
@@ -0,0 +1,35 @@
+There are 3 types of mounts.
+a. Basic Mount
+b. Power Mounts
+c. Sprint Mounts
+
+Basic Mount its a mount just for attack. Your are not able to use skill from it.
+There are 3 types of Basic Mount
+- Lion - 150 Attack
+- Tiger - 100 Defence
+- Wolf	- 10 absorption Pv + PM
+This mounts have no skin. Time for it it's unlimited.
+Drop in map2 and map3 from metin stones.
+Use : from lv.1 
+
+Power Mount are an evolution of mounts and you can attack from it and use horse skill too , but once they evolve.
+Once you drop these mounts ,need to evolve because are useless.Need to go to Horse Keeper and he ask for 2 items:
+a. Pergament of Exorcism - mount evolve with it. Once the mount evolve you are able to attack from it.
+b. Crystal of Fire - with this , create for your mount ability to do some bonuses.
+This scrolls find in Monkey's Dungeons. These seals can be used from lv. 140 and the mounts have a standard period of 480 hours time limit before the seal disappears. The time its used just if you mount on seal.
+To get a power mount need to farm in Monkey Dungeon and kill the boss or farm at lv 130+ metins.
+- Boar - 20% strong again monsters
+- Leopard	- 15% strong again half-human
+- White Lion - 20% exp
+- Reindeer - attack speed + 25 and 10% item drop.
+This mounts have skins and have drop for it at the bosses.
+Use : from lv. 90
+
+Sprint Mount it's a mount with a huge speed of movement.Are used to travel in long distance. There are able to perform skills same with horse skill.
+Have a standard period of 240 hours time limit before the seal disappears. The time its used just if you mount on seal.
+- Arctic Bird - +100 Movement Speed
+- Dino - +100 Movement Speed
+This mount have skins.
+Use : from lv. 140
+
+Mounts have a status bar with resistance for movement.Once the resistance its below 10% will go slow. The Sprint Mount haven't this problem, they have unlimited movement speed.
