312fb1a48c1488d96cc3babaa2a510b9645c162a
Ambreik <gr3bla@gmail.com>
2025-09-26 17:44:51 +0100

    Guardian - files concerning "Guardian" mechanics and description

---
+++ GAME/Guardian/Mechancics.lua
@@ -0,0 +1,10 @@
+			Mechancics 
+	
+	Morale Boost
+Health and Energy are boosted by high morale.
+		Definition
+	A Morale Boost is an effect that increases your base Energy and Health by one to ten percent. It is not applied to energy or health from equipped armor or weapon 
+sets, but is applied to the bonus energy from Energy storage. Morale boost can be typically acquired various ways such, completing various goals during a chain-quest 
+or using certain consumables.
+
+	Link : https://wiki.guildwars.com/wiki/Morale_Boost
