d9c25f4dd723cacd0e8408bc7222400cd88cdf45
Ambreik <gr3bla@gmail.com>
2025-09-26 17:54:50 +0100

    Potion and Food - All files concerning "Potion and Food" mechanics

---
+++ GAME/Potion and Food/Potions.lua
@@ -0,0 +1,5 @@
+			Potions 
+			
+*potions that give the same bonus or attribute, cannot be used again until the effect of the preview is gone (e.g. "Attack Speed Potion" once used, cannot be used 
+again in the next 10 minutes until the effect of thefirst potion is gone, but if a higher "Attack Speed Potion" is used, can replace the effect of a lesser potion)
+*
