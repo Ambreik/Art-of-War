9121a5b052dfaebc2e509f2ee35e57ae05301a3a
Ambreik <gr3bla@gmail.com>
2025-09-26 18:21:37 +0100

    Add files via upload

---
+++ Order and Clan/Guild System.lua
@@ -0,0 +1,175 @@
+			Guild
+			
+	A guild is an in-game association of player characters. Guilds are formed to make grouping and raiding easier and more rewarding, as well as to form a social 
+atmosphere in which to enjoy the game.
+		Background : 
+	Guilds offer many benefits including free items, opportunities for groups, access to trade skill masters, quest items, and readily available trade skill 
+ingredients through gathering guild members. You may discover that a guild greatly enhances your gameplay experience. You can meet friends, share adventures, and 
+find people to protect you if you fight in faction versus faction combat. Typically, players in good guilds can reach content that those not in a guild cannot. A 
+good example of this is high level raiding content and dungeons. end 
+	Keep in mind that guilds are run by players and not "Game Admins". The quality of the guild and the guild experience depends entirely on the players in that 
+guild. Guilds can be a grab bag where you never know what you'll get. Every guild is different.
+	Finding a guild can be very easy. People often sit in town asking anyone to join their guild because guilds require a minimum number of members to create. 
+However, finding a quality guild with quality members and leadership can be a difficult task. Don't be afraid to shop around. You can join one guild, try it out, 
+then leave and join another guild until you find one you like.
+	NOTE: You can move from guild to guild, but this will not enhance your reputation as time goes on. Sometimes the best thing to do is try to make your guild 
+better and leave if that fails. Guilds will be more reluctant to invite you if you have the reputation of joining, taking what you need, and leaving for the next 
+guild that has something better. end end 
+	
+		Advantages of a guild : 
+*Having a group of people willing to help, and on later levels, being able to do high-end instances and mega-dungeons or Guild dungeon. Also finding Battle Groups 
+will be much much easier.
+*Having a group of people to socialize with when you're bored.
+*Guild members are often a more reliable and more kind source of information than general chat.
+*A guild bank lets you exchange items not right for your class or style with ones you prefer or simply help out those less fortunate.
+*Guilds are a great way to improve your "World Game" experience.
+
+		Finding a guild
+	There are many ways to find a Guild. Here are a few spots to check out:
+*Join the "Order Finder Window" or "Clan Finder Window" in game.
+*Watch the "Global Channel" in game for recruiting guild messages.
+*Check the Forums.
+
+		Choosing a guild
+	Guild size
+Guild size can be a big factor in choosing a guild. Guilds that invite too many people, that don't pick quality members, and invite new people all the time are 
+generally less organized and less powerful. However, it can be much easier to find people to group within a larger guild. Smaller guilds are more personal. One 
+complaint that players have about large guilds is that you often run into guild members you don't know ("I don't know all these people."). With a smaller guild 
+there is much more opportunity to get to know each member. For that reason, some players prefer a small guild. Ultimately, the "Large vs. Small" debate depends on 
+the guild and the members that join. There are large guilds that still remain very good. Leadership and recruitment policies are the main factors in determining how
+a guild turns out.
+
+Q: What do you mean when you say small guild?
+A: Most people consider a small guild one with less than 100 members. A guild with less than 50 members is getting really small, but most guilds start out that way.
+Q: What do you mean when you say large guild?
+A: Most people consider a large guild one with more than 100 members (usually more than 150). A cohesive, large guild can be a very good thing, but many large 
+guilds have an active core and a bunch of loners.
+Q: What are the advantages of being in a small or large guild?
+A: Being in a small guild generally has a more tight knit community and more personal connections and better friends. But the same can be said for a big guild, and 
+big guilds have shorter wait times for finding a group. Basically, just go where you feel at home and have fun. end end
+
+		Active members
+	Guild size is less important than the amount of regularly online active members. Some small guilds have more active members than large guilds. A big problem 
+with guilds as they grow is the abundance of alts that pump up the guild size, but not the active membership. You want a guild with lots of active members, since 
+that increases opportunities within the guild. Ideally, a guild you join will have active members in your level range.
+
+		Grouping
+	Grouping can be a good way to find a guild. Team up with people and play with them for a while. Make friends with them and play together for several days. If 
+they get to like you, they will usually want you in their guild so that you'll continue to play with them. After helping them out, inquire about their guild. 
+Perhaps they may help you join.
+
+		Reputation
+	Do your own research into a specific guild. As guilds become powerful, well known, and popular, rumors about them begin to appear. Often when a guild member 
+does something to offend a player, that offended player starts spreading stories about the entire guild. Jealous players make untrue generalizations about the guild
+and its members. Make sure you find out the real story on your own with whatever evidence you can find such as screenshots or web reports. Sometimes a guild may be 
+good and is just the victim of a bad reputation. Guilds can be very jealous or hostile toward each other and thus make up rumors to hurt the reputation of the other
+guild. Or, perhaps, the reputation about that guild is true. Maybe the guild is full of troublemakers. Find out yourself.
+	Large guilds who don't keep good tabs on and train their members to behave often suffer from poor reputations because you don't know what to expect from their 
+members. Generally it isn't a good idea to join a guild that doesn't maintain some standard of behavior because your reputation can get stained by some immature 
+jerk who isn't brought in line by guild leadership.
+	Another point to take away is that what you do while in a guild may reflect on the reputation of your guild and may get you kicked out. You should try to find a
+guild that fits your play style. It's hard to believe, but some guilds form on the basis of being a bunch of jerks; however, most don't. So be aware of the image a 
+guild wants to project.
+
+		Diversity
+	Although some guilds form based on specific race or class affiliations, like "Elf Druids of Elune", most guild have a variety of races and classes. The best 
+guilds have not only a variety of classes (race isn't so important to gameplay due to the nature of powergaming), but also many different professions and play 
+styles. If you like PvP, you should look for a guild that focuses on that play style. If you're into making things, then a guild with a variety of trade skills 
+should be sought. You may need specific classes to fill needs for doing quests in a dungeon or instance, so a guild with a diversity helps with all sorts of 
+situations.
+
+		Positive guild behavior
+	Things to consider when looking for a guild : 
+		
+		Good Grouping
+	One of the most important things you need is the ability to find groups to help you complete your quests. That's why most people join guilds. Guild members are 
+generally much more reliable than strangers, since honesty is a better policy in the long term. Running a Raid or Battleground with a pick-up group can be difficult
+or frustrating due to each player acting in his/her own self interest, a frequent lack of communication, and the general challenges of dealing with strangers.
+		
+		Have good leadership
+	A guild needs clear leadership and strong rules. Members need someone who can resolve conflicts and give direction to members.
+	
+		Generous
+	Look for guilds whose members are willing to share items and help you get things. You shouldn't expect free handouts for no effort. A quality guild will offer
+items and discounts to their members. Futhermore, a guild that values generosity will have little or none of the issues discussed under Drama below.
+Similarly, high-level players often run through lower-level content for fun. Such players may even invite level-appropriate characters to come along for easy EXP or
+loot. Again, this is best viewed as a happy accident, not as something they owe you; guild veterans have their own business to take care of, and will quickly tire 
+of somebody who constantly begs for favors. Conversely, you'll probably receive more invitations if you build a reputation for being friendly, sharing what you 
+have, and generally pulling your own weight.
+	
+		Fairness
+	Some guilds show favoritism towards their core members (usually a difficult to determine and amorphous collection) or friends or relatives regardless of their 
+behavior. Other guilds only pay attention to their high level members and generally let the lower level members struggle until they make it to a high enough level. 
+You should look for a guild that treats its members fairly and as evenly as possible. Since you will be a new member, the general pattern puts you at the bottom of 
+the totem pole, so you don't want to be in a guild that treats you like dirt for no good reason. The best guilds help you level, but don't twink too much, so they 
+know you're not taking advantage.
+		
+		Low Spam
+	Guild chat is a very important part of a guild. Guild members typically use this method to communicate. Find a guild that has good guild chat rules and enforces
+them. Otherwise, you might have to put up with a lot of garbage text while you play. There are advanced chat options that allow you to turn off guild chat; however,
+this might cause you to miss out on fun, spur-of-the-moment events.
+
+		Drama
+	Item fights and guild members fighting with each other are common problems in guilds. If the guild has good leaders and quality members, these problems can be 
+reduced or dealt with when they arise. Sometimes players quit a guild because of "too much drama." Games are supposed to be fun. If time is spent fighting, that's 
+not very much fun. Don't be afraid to leave a guild (and if you're a guild leader, don't be skittish about kicking a member that's prone to this behaviour, despite 
+personal ties) and you'll save yourself from more headaches in the future (and members if you're a guild leader). There will always be other guilds to join and you 
+can always create your own (and always recruit new members).
+	
+		Mature Leaders
+	When players get power such as the ability to remove guild members, demote them, and tell them what to do, they sometimes abuse that power. Find a guild whose 
+leaders are responsible with the power they have.
+	
+		Applications
+	Many times a guild will request that you put an application on their guild site before they offer you a guild invitiation. This is particularly the case with 
+endgame PvE guilds, who require a pool of skilled, intelligent players to progress through raid content. Just because a guild requires an application doesn't mean 
+that they are an elitist or exclusive group. Often it is seen simply a routine requirement for any potential new member, or a way to quickly introduce them to 
+current guild members.
+
+	Guild applications vary wildly, but almost all will ask for your character name, class, level, and guild history. Many will also ask about your real-life age 
+and gender, specific instances you are attuned or keyed for, raiding experience, hobbies or interests outside of WoW. In general it is a good idea to be as specific
+and thorough as possible on a guild application. Unless you already have grouped with or otherwise know current members of the guild, this will probably be their 
+first introduction to you and if you are serious about becoming part of the guild you will want to make an impression that will improve your chances of getting an 
+invite. end 
+	Some guild leaders and officers look for specific things on an application (and there can also be specific things they watch out for) and in general a guild 
+application is examined closely to determine whether the applicant would be a good addition to the guild as a whole. But the best policy is simply to be yourself; 
+if you present yourself as someone who you aren't, your new guild will probably find out quickly (and you probably can find another guild that you will be happier 
+in). end end end end end end end end end 
+	Sometimes guilds allow multiple people to apply as a group for the guild (often because they are real-life friends), but often they ask that you do not. If they
+do not, or if it is not clear whether they do, it is a bad idea to put in a group application. If you truly want to join a guild as a group, the best way in this 
+situation is for one person to join the guild and let them get to know that person. If leaders determine that this first applicant is a good addition to the guild, 
+they will often invite other people because they know that person. Even if this does not happen, you always have the option of leaving the guild in order to find a 
+place that you and your friends are all welcome. end 
+
+		Conclusion
+	Do some research, shop around, and keep trying until you find a good guild. Don't give up. There are good guilds out there. You just have to find them. 
+	Remember, you can always create your own guild if you want and make one that follows your own requirements.
+
+		Leaving a guild
+Type /gquit.
+(It is a good idea to let the guild leader know why you are leaving first.) If you are the guild leader and want to disband Type /gdisband
+
+		Starting a guild
+	Talk to a guild master NPC in a major city. A charter can be purchased for 10G. You will need 4 other Players to sign the charter before you can turn it in and 
+start your guild. Choosing a tabard design costs 20G, but the guild can be created without paying for the tabard design. When you still have the guild charter you 
+can change the guild name to something that isn't taken.
+	Guild names are case sensitive but cannot be same name spelled with lower or capital letter, on the same server.
+Note: When signing a guild charter, once a player's character signs, no alternate characters on that account may sign. The charter must have signatures from 4 
+other Players, each on a different account. This is why people sometimes offer to pay to have their charters signed. Yet you can have different character from the 
+same account join the guild after charter is turned in.
+	Once you begin getting signatures on your charter, focus on getting the remaining signatures on your charter and registering the guild. Signatures can disappear
+if you delay, since players are allowed to put signatures on other charters until you turn yours in. Keep in mind that you need at least 10 signatures before you 
+areable to create it.
+
+		Guild tabards and bank tabs
+	Each guild can design their own crest. This design is featured on tabards which all guild members can wear, as well as Guild Battle Standards, and is used to 
+represent the guild in places such as guild members' Guild tab, and on the official "Art of War" website. The guild leader designs the tabard for a fee of 10g 
+from the Guild Master NPC found in any major city. In some cities, there is a separate Guild Tabard Designer NPC standing nearby.
+	A player can purchase a tabard to wear for 1g from a guild tabard vendor, found near the Guild Master NPC. If you are not in a guild, you may still purchase and
+wear a tabard, but it will be grey until you join a guild that has already chosen their design. The tabard is a visual symbol or badge of membership for each guild. 
+	The only purpose beyond the look is to display your guild or faction pride, otherwise tabards provide no mechanical benefit to your character. Once a tabard is 
+designed, you must pay another 10g each time to change it, so choose wisely the first time around.
+	More expensive than tabards, guild banks provide space for members to share items, much like a "multi-player bank." The Guild Leader can limit the players who 
+can add and/or remove items from the bank. It features six purchasable tabs which have 90 item slots each; however, unlike personal banks, guild banks do not 
+support the addition of loaded bags. The price for the first tab begins at 100g, then increases for each successive tab, 250g, 500g, 1,000g, 2,500g, 5,000g, for a 
+grand total of 9350g. Other than items, gold can be stored and used for repairs, pay and upgrade NPCs or to create buildings.
---
+++ Order and Clan/Vote Kick.lua
@@ -0,0 +1,48 @@
+			Vote Kick
+
+	The Vote Kick system is a tool used in "Order/Clan" where members vote against another member to be kicked out. 
+	Using the Vote Kick feature too often will not allow a player to initiate a Vote Kick for 2 hours for the entire "Order/Clan" members.
+	
+		Mechanic
+	Anyone in the "Order/Clan" can initiate a "Vote Kick." When someone wants to remove a player from their group, they right-click on the player's name in the 
+"Order/Clan" UI, and selects "Vote to kick". A message window appears, prompting the initiating player to enter a reason for the kick. This reason is presented to 
+the other members with the option to accept the reason and kick the player, or deny the reason and keep the player in the "Order/Clan". This message comes as pop-up 
+to all online members wherever they are. All offline members are informed by this decision once they are online via message and they have to go to "Order/Clan" 
+"News" option and choose. Members can refuse to vote aswell but the Leader have a decisive role before all members vote.
+	Once a vote to kick a member out is done by a member of the "Order/Clan" , the once is informed by a message by system announcing him regarding the reason chosen
+and also who initiate. In this scenario, the kicked player cannot access the store of the organisation at all even if he/she stored gold or items there. Although 
+in a case where the decision is against he/she, all the stored goods come back via "Mail" after one hour being kicked out. All this goods have to be store at least 
+one day before or 5 days, older than 5 days the system cannot count it and are lost. 
+	Once the decision was made it will be open for 24hours time when all members have to vote or refuse to vote. When all was online and voted, the Leader must also 
+to vote and the leader vote represent 40% and can also determine the future to a member. end
+	
+		Limits
+*A vote can be done every 48 hours.
+
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	armura mobi - 20-25w
+	jad antic - 1,3w
+	zodiaca - 5-8w
+	colier - 30-40w
+	bratara lc 15 fulger 20 orci - 7-8w
