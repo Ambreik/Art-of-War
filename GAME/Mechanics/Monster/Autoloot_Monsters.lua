eca2a4138e72e51594ecefb044f5308133d7028c
Ambreik <gr3bla@gmail.com>
2025-10-04 18:42:43 +0100

    Delete GAME/Mechanics/Monster directory

--- GAME/Mechanics/Monster/Autoloot_Monsters.lua
+++
@@ -1,8 +0,0 @@
-			Autoloot NPC
-
-- Description :  
-	Specific NPCs tied to autoloot mechanics include:  
-	- Monsters and NPCs that drop loot.  
-	- Quest-giving NPCs offering Magic Hand or upgrades as rewards.  
-	- Vendors selling autoloot-enabling items or consumables.  
-- Special Mechanic : Some NPCs can temporarily enhance autoloot range via buffs or special rewards.  
--- GAME/Mechanics/Monster/Monster_Spell.lua
+++
@@ -1,5 +0,0 @@
-			Monster_Spell
- 
-	Description : This section covers how monsters use spells. Monster spells include offensive and defensive abilities based on their type and rank. 
-They can deal damage, apply debuffs, or even heal allies. Monster spells can have varying casting times, and some may require specific conditions 
-(e.g., a boss monster requiring a corpse to cast a powerful spell).  
