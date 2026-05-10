c5558175363333274e82f78f94418de6c59879e7
Ambreik <gr3bla@gmail.com>
2026-05-10 10:08:32 +0100

    Delete NPCs/Middlean Islands - Lv 30 - 125/Capital City/Maggor Ren directory

--- NPCs/Middlean Islands - Lv 30 - 125/Capital City/Maggor Ren/SHOP.lua
+++
@@ -1,4 +0,0 @@
-			SHOP
-			
-*Swimming Potion : increase swimming by +15 for 5 minutes. 
-*Swimming Suit : equipped as an armor. Offer the ability to swim deeper and swimming speed increased by 30% for 1 hour.
